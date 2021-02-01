cf25c2c79384a1eb5ed22b1f185b5249d711cd4c
Thiago Jung <30575561+Thiagojm@users.noreply.github.com>
2021-02-01 20:07:44 -0300

    Create secrets_rng.py
    One more option?

---
+++ secrets_rng.py
@@ -0,0 +1,12 @@
+import secrets
+
+
+bit_size = 2048
+bits = secrets.randbits(bit_size)
+
+print(bits)
+bin_ascii = bin(bits)[2:].zfill(bit_size)
+print("Binary String: ", bin_ascii)
+print("Binary lenght: ", len(bin_ascii))
+num_ones_array = bin_ascii.count('1')
+print("Number of 'ones': ", num_ones_array)
