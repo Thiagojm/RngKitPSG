88301880acb90aec040e2c7f5d14aab1c96e468b
Thiagojm <tjm.plastica@gmail.com>
2021-09-12 12:52:48 -0300

    up

--- tests/teste1.py
+++ tests/teste1.py
@@ -1,5 +1,21 @@
-import pandas as pd
+import os
+import glob
+import pathlib
+from contextlib import ExitStack
 
-teste_file = pd.read_csv(r"D:\Meus Documentos\1-Projetos\RngKitPSG\1-SavedFiles\2019_teste.csv")
-print(teste_file.head())
-print(teste_file.)
+os.chdir(pathlib.Path(r"C:\Users\tjmpl\Documents\Projetos\RngKitPSG\1-SavedFiles"))
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+print(all_filenames)
+
+# for i in all_filenames:
+#     print(i)
+
+with ExitStack() as stack:
+    files = [stack.enter_context(open(fname)) for fname in all_filenames]
+    with open("concat_csv.csv", "a") as f:
+        for file in files:
+            for line in file:
+                f.write(line)
+                
