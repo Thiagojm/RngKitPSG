c1db859a2650e4b2499dc14f6d8ba6459386b42b
Thiagojm <tjm.plastica@gmail.com>
2022-02-23 12:46:05 -0300

    rngkit 2.2

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ Do not close this window!""")
         tab_location="top", font="Calibri, 18")]]
 
     # WINDOW
-    window = sg.Window("RngKit ver 2.1.7 - by Thiago Jung - thiagojm1984@hotmail.com", layout, size=(1024, 720),
+    window = sg.Window("RngKit ver 2.2 - by Thiago Jung - thiagojm1984@hotmail.com", layout, size=(1024, 720),
                        location=(50, 50), finalize=True, element_justification="center", font="Calibri 18",
                        resizable=True, icon=("src/images/BitB.ico"))
 
--- tests/nsis_maker.py
+++ tests/nsis_maker.py
@@ -9,12 +9,12 @@ appname = "RngKit"
 compname = "Conscienciology"
 descript = "This application uses two types of TRNGs - True Random Number Generators (TrueRNG and Bitbbabler) for data collection and statistical analysis for several purposes, including mind-matter interaction research."
 vmajor = 2
-vminor = 1
-vbuild = 6
+vminor = 2
+vbuild = 0
 helpurl = ""
 updateurl = ""
 abouturl = ""
-installsize = 123669
+installsize = 126533
 iconpath = "src/images/BitB.ico"
 builder = "py"
 
